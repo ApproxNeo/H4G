75088b5d51735df9f987324b0523c9079a269f34
potty10 <christophertan8664@gmail.com>
2023-02-18 11:35:26 +0800

    Fix footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import './styles/App.css';
 import NavBar from './components/Navbar';
 import Footer from './components/Footer';
 import AboutMeScreen from './screens/AboutMeScreen';
@@ -8,13 +7,15 @@ import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <div style={{display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
         <NavBar/>
-        <Routes>
-          <Route path="/" element={<AboutMeScreen/>}/>
-          <Route path="/problems" element={<ProblemScreen/>}/>
-          <Route path="/projects" element={<ProjectScreen/>}/>
-        </Routes>
+          <div style={{flex: 1, flexGrow: 1, height: '100%'}}>
+            <Routes>
+              <Route path="/" element={<AboutMeScreen/>}/>
+              <Route path="/problems" element={<ProblemScreen/>}/>
+              <Route path="/projects" element={<ProjectScreen/>}/>
+            </Routes>
+          </div>
         <Footer/>
     </div>
   );
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import "../styles/Footer.css"
 
 export default function Footer() {
   return (
-    <footer>
+    <footer style={{flex: 0}}>
         Copyright @ Christopher Tan, Clement Neo
     </footer>
   );
